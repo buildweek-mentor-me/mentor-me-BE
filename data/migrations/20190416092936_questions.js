1fc75a914238b698703452fe604a6b7ab013e715
dislersd <dislersd@gmail.com>
2019-04-16 10:08:50 -0700

    switched development db to postgres - building questions model

---
+++ data/migrations/20190416092936_questions.js
@@ -0,0 +1,23 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("questions", questions => {
+    questions.increments();
+    questions.string("title", 128).notNullable();
+    questions.string("body", 128).notNullable();
+    questions
+      .timestamp("created_at", { useTz: true })
+      .defaultTo(knex.fn.now(6));
+    questions.string("author").notNullable();
+    questions.integer("likes");
+    questions
+      .integer("FK_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("questions");
+};
