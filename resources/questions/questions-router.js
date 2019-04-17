959d343bab097eb85e86214cc4734e6e29d52314
dislersd <dislersd@gmail.com>
2019-04-16 21:23:55 -0700

    created questions put route - need to test

--- resources/questions/questions-router.js
+++ resources/questions/questions-router.js
@@ -36,6 +36,16 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const changes = await Questions.update(id, req.body);
+    res.status(200).json(changes);
+  } catch (error) {
+    res.status(500).json({ message: 'error', error })
+  }
+})
+
 router.delete('/:id', async (req, res) => {
   try {
     const {id} = req.params;
