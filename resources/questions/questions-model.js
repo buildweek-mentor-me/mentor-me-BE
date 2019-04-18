8d2b94ed51b0c5ace79bff38d5b686ea11daa2fd
dislersd <dislersd@gmail.com>
2019-04-18 12:35:51 -0700

    created like route to increment likes

--- resources/questions/questions-model.js
+++ resources/questions/questions-model.js
@@ -39,6 +39,17 @@ module.exports = {
       .update(changes)
       .then(count => (count > 0 ? this.get(id) : null));
   },
+  updateLikes: function(id) {
+    // return db("questions")
+    //   .where({ id })
+    //   .returning("id")
+    //   .update({ likes: knex.raw("likes + 1") })
+    //   .then(count => (count > 0 ? this.get(id) : null));
+    return db("questions")
+      .where({ id })
+      .increment("likes", 1)
+      .then(count => (count > 0 ? this.get(id) : null));
+  },
   remove: function(id) {
     return db("questions")
       .where({ id })
