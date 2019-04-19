1517b4d7482796f18e0844f4d47fe0d5edc74a1b
dislersd <dislersd@gmail.com>
2019-04-18 21:09:54 -0700

    importing firebase

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const helmet = require('helmet');
 const cors = require('cors');
+const admin = require('firebase-admin')
+// firebase
+const serviceAccount = require('../mentor/service.json')
 // server
 const server = express();
 // routers
@@ -17,6 +20,17 @@ server.use('/auth', authRouter);
 server.use("/users", usersRouter);
 server.use("/questions", questionsRouter);
 server.use("/answers", answersRouter);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mentor-mee.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
 
 server.get("/", (req, res) => {
   res.send("Hello World");
