b0a3eada9aab8bfd1ca7a5f6284c2af7fc6cef25
dislersd <dislersd@gmail.com>
2019-04-18 22:26:24 -0700

    updated readme

--- api/server.js
+++ api/server.js
@@ -1,22 +1,22 @@
 // libraries
 const express = require("express");
-const helmet = require('helmet');
-const cors = require('cors');
-const admin = require('firebase-admin')
+const helmet = require("helmet");
+const cors = require("cors");
+const admin = require("firebase-admin");
 // firebase
-const serviceAccount = require('../mentor/service.json')
+const serviceAccount = require("../mentor/service.json");
 // server
 const server = express();
 // routers
-const authRouter = require('../auth/auth-router')
+const authRouter = require("../auth/auth-router");
 const usersRouter = require("../resources/users/users-router");
-const questionsRouter = require('../resources/questions/questions-router')
-const answersRouter = require('../resources/answers/answers-router')
+const questionsRouter = require("../resources/questions/questions-router");
+const answersRouter = require("../resources/answers/answers-router");
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-server.use('/auth', authRouter);
+server.use("/auth", authRouter);
 server.use("/users", usersRouter);
 server.use("/questions", questionsRouter);
 server.use("/answers", answersRouter);
@@ -24,12 +24,6 @@ server.use("/answers", answersRouter);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://mentor-mee.firebaseio.com"
-});
-
-var db = admin.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
 });
 
 server.get("/", (req, res) => {
